313724cc0bbc1a53bf27b6e9a589a4c4c9e4603d
Ghaffarsain <161946946+Ghaffarsain@users.noreply.github.com>
2024-03-12 20:39:58 +0500

    Add files via upload

---
+++ 12.Names.ts
@@ -0,0 +1,11 @@
+
+
+let names :string[] = ['Abdul Ghaffar','Rehman','Yasir','Arshad'] ;
+
+const message :string = 'wlecome to Solangi House: '
+
+for(let i=0; i<names.length; i++){
+
+    console.log(message + names[i]) ;
+    
+}
