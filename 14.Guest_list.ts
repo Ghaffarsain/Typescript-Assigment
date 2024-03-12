e6820a86c912d2af1c8983f912b7f55e5b31364e
Ghaffarsain <161946946+Ghaffarsain@users.noreply.github.com>
2024-03-12 20:43:10 +0500

    Add files via upload

---
+++ 14.Guest_list.ts
@@ -0,0 +1,9 @@
+
+
+let Guest_list :string[] = ['zain Memon','Murtaza Lighari','Zuhaib Mallah'];
+
+for (let i=0; i<Guest_list.length; i++){
+
+    console.log('Dear Mr. ' + Guest_list[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You!')
+
+}
