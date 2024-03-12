4a92d7d8be294ece2fdf930409d1709d0c59acf6
Ghaffarsain <161946946+Ghaffarsain@users.noreply.github.com>
2024-03-12 20:36:13 +0500

    Add files via upload

---
+++ 10.Programe_1.ts
@@ -0,0 +1,8 @@
+
+
+// programe written by: Abdul Ghaffar
+// current date: 26.2,2024
+
+// this programe will write Hello world
+
+console.log('hello world');
---
+++ 10.programe_2.ts
@@ -0,0 +1,8 @@
+
+
+// programe written by: Abdul Ghaffar
+// current date: 26.2,2024
+
+// this programe will add two digits.
+
+console.log(2+2);
